f3f1561f0492df645128951b7a6248c308951952
rbgo404 <150957746+rbgo404@users.noreply.github.com>
2023-11-20 14:50:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import torch
 
 class InferlessPythonModel:
     def initialize(self):
-        tokenizer = AutoTokenizer.from_pretrained("codellama/CodeLlama-7b-hf")
-        pipeline = transformers.pipeline(
+        self.tokenizer = AutoTokenizer.from_pretrained("codellama/CodeLlama-34b-Python-hf")
+        self.pipeline = transformers.pipeline(
             "text-generation",
             model="codellama/CodeLlama-34b-Python-hf",
             torch_dtype=torch.float16,
@@ -16,14 +16,14 @@ class InferlessPythonModel:
     def infer(self, inputs):
         print("inputs[prompt] -->", inputs["prompt"], flush=True)
         prompts = inputs["prompt"]
-        sequences = pipeline(
+        sequences = self.pipeline(
             prompts,
             do_sample=True,
             temperature=0.2,
             top_p=0.9,
             num_return_sequences=1,
-            eos_token_id=tokenizer.eos_token_id,
-            max_length=100,
+            eos_token_id=self.tokenizer.eos_token_id,
+            max_length=512,
         )
         for seq in sequences:
             return {"result": seq['generated_text']}
