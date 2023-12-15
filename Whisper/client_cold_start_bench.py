18f73d04153f6bd818207fbc0a47318d35f0a008
RajdeepBorgohain <rajdeepnewgeneration@gmail.com>
2023-12-15 10:17:33 +0000

    Added the cold-start test

---
+++ Whisper/client_cold_start_bench.py
@@ -0,0 +1,63 @@
+import requests, json, time
+from transformers import AutoTokenizer
+import pandas as pd
+import argparse
+
+parser = argparse.ArgumentParser(description='Run LLM inference.')
+parser.add_argument('--model_name', type=str, required=True, help='Model name for inference.')
+#parser.add_argument('--quantization', type=str, required=True, help='Quantization to be use for quant.')
+args = parser.parse_args()
+
+print("Starting cold start test for ",args.model_name.split('/')[-1])
+tokenizer = AutoTokenizer.from_pretrained(args.model_name)
+
+def chat(prompt:str):
+    payload = {
+  "inputs": [
+    {
+      "data": [
+        "https://huggingface.co/datasets/Narsil/asr_dummy/resolve/main/2.flac"
+      ],
+      "name": "audio_url",
+      "shape": [
+        1
+      ],
+      "datatype": "BYTES"
+    }
+  ]
+}
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post("http://localhost:8000/v2/repository/models/model_auto_gptq/unload")
+    time.sleep(3)
+    start = time.perf_counter()
+    response = requests.post("http://localhost:8000/v2/repository/models/model_auto_gptq/load")
+    start_inference_time = time.perf_counter()
+    response = requests.post("http://localhost:8000/v2/models/model_auto_gptq/infer", headers=headers, data=json.dumps(payload))
+    print(response.json())
+    load_time = start_inference_time - start
+    inference_time = time.perf_counter() - start_inference_time
+
+    request_time = time.perf_counter() - start
+
+    print(f"Inference time: {inference_time}, Load Time: {load_time},Total time: {request_time}")
+    generated_text = response.json()['outputs'][0]['data'][0]
+    print(generated_text)
+    return {
+        #'tok_count': len(tokenizer.encode(generated_text)),
+        #'cold_start_time': request_time,
+        'cold_time':load_time,
+        'inference_time':inference_time,
+        #'question': prompt,
+        'answer': generated_text,
+        'note': 'triton-auto-mixtral'}
+
+
+if __name__ == '__main__':
+    responses = []
+    for i in range(10):
+        prompt = "Write a good poem."
+        response = chat(prompt)
+        responses.append(response)
+
+    df = pd.DataFrame(responses)
+    df.to_csv(f'bench-cold-start-whisper.csv', index=False)
